24ba22525b4f4705163b6ebb2954e7cbbfd20f04
Greg Cohn <greg.cohn@nau.edu>
2026-05-27 10:54:06 -0700

    DEVELOP: create data path inputs for use in stand alone function and make number of bins flexible to data

--- i-dash/server.R
+++ i-dash/server.R
@@ -14,18 +14,47 @@ library(glue)
 library(purrr)
 library(dplyr)
 
-intelimon_out <- read_yaml('../data_loc.yaml')$data_dir
-data_dir <- file.path(intelimon_out, 'metrics')
-
-
+hide_data_input_tab <- file.exists('../data_loc.yaml')
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
   
+  # Immediately decide to display data path or not
+  observeEvent(session$onFlushed,{
+    if (hide_data_input_tab) {
+      hideTab(inputId = "load_data", target = "data_input_tab")
+    } else if(!hide_data_input_tab){
+      showTab(inputId = "load_data", target = "data_input_tab")
+    }
+  })
+  
+  # Initialize data_dir reactively based on yaml existence
+  data_dir <- reactiveVal({
+    
+    if (hide_data_input_tab){
+      intelimon_out <- read_yaml('../data_loc.yaml')$data_dir
+      file.path(intelimon_out, 'metrics')
+    } else{
+      NULL
+    }
+  })
+  
+  # update data_dir if input through the UI.
+  observeEvent(input$submit_dir, {
+    req(input$ui_data_dir)
+    
+    if(dir.exists(input$ui_data_dir)){
+      data_dir(input$ui_data_dir)
+    }else{
+      showNotification("Directory does not exist.", type = "error")
+    }
+  })
+
   # Reactive expression to fetch raw file names and create clean labels
   file_select <- reactive({
+    req(data_dir())
     
-    file_names <- list.files(data_dir, pattern = "\\.csv$")
+    file_names <- list.files(data_dir(), pattern = "\\.csv$")
     
     plot_names <- tools::file_path_sans_ext(file_names)
     
@@ -35,6 +64,7 @@ function(input, output, session) {
   
   # Dynamically render the selection UI based on available files
   output$plot_selector_ui <- renderUI({
+    req(data_dir())
     plot_files <- file_select()
     
     if (length(plot_files) == 0) {
@@ -53,18 +83,19 @@ function(input, output, session) {
   # Reactive expression to load and combine the selected CSV data
   combined_data <- reactive({
     req(input$selected_plots)
+    req(data_dir())
+    
     plot_files <- file_select()
     
     # Map user selection back to actual file names and build full paths
     selected_files <- plot_files[input$selected_plots]
-    full_paths <- file.path(data_dir, selected_files)
+    full_paths <- file.path(data_dir(), selected_files)
     
     # Read all selected files and merge them into a single data frame
     full_paths %>% 
       setNames(input$selected_plots) %>% 
       map_df(~read.csv(.x), .id = "Plot_ID")
   })
-  
   
   output$column_selector_ui <- renderUI({
     req(input$selected_plots)
@@ -76,14 +107,35 @@ function(input, output, session) {
       inputId = "selected_column",
       label = "Choose Column:",
       choices = names(df),
-      selected = names(df)[1]
+      selected = names(df)[-1]
     )
   })
   
+  output$bins_ui <- renderUI({
+    req(combined_data(), input$selected_column)
+
+    df <- combined_data()
+    col <- input$selected_column
+
+    x <- df[[col]]
+
+    # remove NA
+    x <- x[!is.na(x)]
+
+    req(length(x) > 0)
+
+    # you can tune this logic depending on your data
+    sliderInput(
+      "bins",
+      "Number of bins:",
+      min = 2,
+      max = max(3, length(x)),   # scale with data size
+      value = min(3, length(x))
+    )
+  })
+
   # Dynamically render the plot
   output$col_hist <- renderPlot({
-    print('Plot triggered')
-    print(input$selected_column)
     req(combined_data(), input$selected_column)
     
     df <- combined_data()
--- i-dash/ui.R
+++ i-dash/ui.R
@@ -14,18 +14,21 @@ fluidPage(
 
     # Application title
     titlePanel("TLS Monitoring"),
+    
+    tabsetPanel(
+      id = "load_data",
+      tabPanel(title = "Load Data", value = "data_input_tab",
+               textInput("ui_data_dir", "Enter Data Directory Path:"),
+               actionButton("submit_dir", "Load Directory")
+               )
+    ),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             uiOutput("plot_selector_ui"),
             uiOutput("column_selector_ui"),
-            
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            uiOutput('bins_ui')
         ),
 
         # Show a plot of the generated distribution
