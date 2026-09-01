9b86f8d513f8524805a036946f3c638f09c78a08
Greg Cohn <greg.cohn@nau.edu>
2026-09-01 16:32:23 -0700

    DEVELOP: allow unit name and remeasurement number to editable.
    Create text input boxes and assignment tabs that allow the user to bulk change selected rows. Also allow individual rows to be edited by the user.

--- app/view/tab_set_trtmt.R
+++ app/view/tab_set_trtmt.R
@@ -28,6 +28,11 @@ ui <- function(id) {
 
         bslib$card(
           bslib$card_header(shiny$h4("Add Treatment Dates")),
+          shiny$helpText(
+            "Scans after entered treatment dates will have their remeasurement numbers
+            automatically increased."
+          ),
+          height = "400px",
           shiny$dateInput(
             ns("ui_selected_date"),
             label = shiny$h6("Select Date"),
@@ -44,10 +49,36 @@ ui <- function(id) {
           DT$DTOutput(
             ns("tbl_trtmt_dates")
           )
+        ),
+
+        bslib$card(
+          bslib$card_header(shiny$h4("Set Unit Name or Remeasurement")),
+          shiny$helpText(
+            "To assign a unit name or remeasurement number, select the scans in the table to the left,
+            enter the desired values below and hit the Assign button."
+          ),
+          shiny$textInput(
+            ns("ui_enter_unit"),
+            label = "Unit Name",
+            placeholder = "BigCreek",
+            value = "",
+            updateOn = "change"
+          ),
+          shiny$textInput(
+            ns("ui_enter_remeas"),
+            label = "Scan Remeasurement",
+            placeholder = "0 (initial scan)",
+            value = "",
+            updateOn = "change"
+          ),
+          shiny$actionButton(
+            ns("btn_assign"),
+            "Assign"
+          )
         )
       ),
       DT$DTOutput(
-        ns("selected_plots"),
+        ns("tbl_selected_scans"),
         height = "100%"
       )
     )
@@ -58,6 +89,7 @@ ui <- function(id) {
 server <- function(id) {
   shiny$moduleServer(id, function(input, output, session) {
 
+    #----Add Treatment Dates---------------------
     shiny$observeEvent(input$btn_add_trtmt, {
       trtmt_dates <- session$userData$trtmt_dates()
 
@@ -88,9 +120,11 @@ server <- function(id) {
           caption = "Treatment Dates",
           filter = "none",
           rownames = FALSE,
+          fillContainer = TRUE,
           selection = "single",
+          editable = TRUE,
           options = list(
-            pageLength = 10,
+            scrollY = "100%",
             ordering = TRUE,
             searching = FALSE,
             paging = FALSE,
@@ -99,10 +133,41 @@ server <- function(id) {
         )
     })
 
+    #------Assign Unit or Remeasurement----------
+    shiny$observeEvent(input$btn_assign, {
+
+      # always initializes as NULL
+      selected_rows <- input$tbl_selected_scans_rows_selected
+      if (is.null(selected_rows)) {
+        shiny$showNotification(
+          "No scans selected. Click on the desired rows in the the table to the left",
+          type = "warning",
+          duration = 30
+        )
+      }
+
+      # get the row for the original sorting (this always seems to be the same as selected_rows)
+      orig_display_row <- input$tbl_selected_scans_rows_all[selected_rows]
+
+      # get the data.table with the original sorting
+      displayed_scans <- dt$copy(session$userData$scan_selection())
+      displayed_scans <- displayed_scans[order(plot, site, -date)]
+
+      if (nzchar(input$ui_enter_unit)) {
+        displayed_scans[orig_display_row, Unit := as.character(input$ui_enter_unit)]
+      }
+
+      if (nzchar(input$ui_enter_remeas)) {
+        displayed_scans[orig_display_row, Remeasurement := as.integer(input$ui_enter_remeas)]
+      }
+
+      session$userData$scan_selection(displayed_scans)
+    })
+
     #---Data Table-------------------------------
     columns <- c('site', 'plot', 'date', 'scanner_id', "Unit", "Remeasurement")
 
-    output$selected_plots <- DT$renderDT({
+    output$tbl_selected_scans <- DT$renderDT({
       selected_scans <- session$userData$scan_selection()
 
       nscans <- nrow(selected_scans)
@@ -130,6 +195,8 @@ server <- function(id) {
         filter = "top",
         rownames = FALSE,
         selection = "multiple",
+        # make site, plot, date, scanner_id ReadOnly, but allow unit and remeasurement to be changed
+        editable = list(target = "cell", disable = list(columns = c(0, 1, 2, 3))),
         options = list(
           pageLength = 50,
           ordering = TRUE,
@@ -138,6 +205,18 @@ server <- function(id) {
           order = list(list(2, "asc"), list(0, "asc"), list(1, "asc"))
         )
       )
+    })
+
+    # allow the user to change table values
+    shiny$observeEvent(input$tbl_selected_scans_cell_edit,{
+      changes <- input$tbl_selected_scans_cell_edit
+
+      selected_scans <- session$userData$scan_selection()
+      displayed_scans <- selected_scans[, ..columns]
+
+      displayed_scans <- DT$editData(displayed_scans, changes, rownames = FALSE)
+
+      session$userData$scan_selection(selected_scans[, (columns) := displayed_scans])
     })
   })
 }
